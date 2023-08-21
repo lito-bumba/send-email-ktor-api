5397b628076e6cc9e8d8d0dc2bb5aa85116fae12
= <=>
2023-08-21 16:07:20 -0300

    Initial Project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    id("io.ktor.plugin") version "2.3.3"
+    kotlin("plugin.serialization").version("1.9.0")
+}
+
+group = "com.bumba"
+version = "1.0-SNAPSHOT"
+application {
+    mainClass.set("com.bumba.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.3")
+    implementation("io.ktor:ktor-server-call-logging:2.3.3")
+
+    implementation("io.ktor:ktor-server-core-jvm:2.3.3")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.3")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    testImplementation("io.ktor:ktor-server-tests-jvm:2.3.3")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.0")
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,22 @@
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(
+        factory = Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/"){
+            call.respond("Initial Project Ktor")
+        }
+    }
+}
